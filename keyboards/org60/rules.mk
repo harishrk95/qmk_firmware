361d950369c6a3a72dc2bdf26b676b66e69e16ff
Ryan Sullivan <RyanSullivan@gmail.com>
2017-08-21 22:22:26 -0400

    Enabled Unicode
    - Going to try and start playing with that again, so turning it on

--- keyboards/org60/rules.mk
+++ keyboards/org60/rules.mk
@@ -61,4 +61,4 @@ MOUSEKEY_ENABLE = yes       # Mouse keys(+4700)
 NKRO_ENABLE = yes           # Nkey Rollover - if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 RGBLIGHT_ENABLE = yes       # Enable WS2812 RGB underlight.  Do not enable this with audio at the same time.
 SLEEP_LED_ENABLE = no       # Breathing sleep LED during USB suspend
-UNICODE_ENABLE = no         # Unicode
+UNICODE_ENABLE = yes        # Unicode
